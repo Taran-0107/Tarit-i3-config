3d8045a1f55015d95a14b568e99373bbd8db3439
Tarandeep Singh <tarandeepsingh399113@gmail.com>
2026-08-24 22:37:39 +0530

    addded rofi themes

--- install.sh
+++ install.sh
@@ -15,6 +15,7 @@ mkdir -p ~/.config/compton
 
 mkdir -p ~/.local/share/fonts
 
+mkdir -p ~/.local/share/rofi/themes
 
 cp $SCRIPT_DIR/configs/polybar.ini ~/.config/polybar/config.ini
 cp $SCRIPT_DIR/configs/kitty.conf ~/.config/kitty/kitty.conf
@@ -23,5 +24,5 @@ cp $SCRIPT_DIR/configs/i3config ~/.config/i3/config
 cp -r $SCRIPT_DIR/configs/wallpapers ~/wallpapers
 cp $SCRIPT_DIR/configs/i3move.sh ~/.config/i3/move.sh
 cp $SCRIPT_DIR/fonts/nerdfont.ttf ~/.local/share/fonts/nerdfont.ttf
-
+cp $SCRIPT_DIR/rofi/material.rasi ~/.local/share/rofi/themes/material.rasi 
 echo "config files installed! (0w0)"
