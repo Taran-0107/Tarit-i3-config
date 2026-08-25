26ae7e393fb869ec5ad02b538b5145f2ee5db9d0
Tarandeep Singh <tarandeepsingh399113@gmail.com>
2026-08-25 14:13:21 +0530

    rofi config added

--- install.sh
+++ install.sh
@@ -19,6 +19,8 @@ mkdir -p ~/.local/share/rofi/themes
 
 mkdir -p ~/.icons
 
+mkdir -p ~/.config/rofi
+
 cp $SCRIPT_DIR/configs/polybar.ini ~/.config/polybar/config.ini
 cp $SCRIPT_DIR/configs/kitty.conf ~/.config/kitty/kitty.conf
 cp $SCRIPT_DIR/configs/compton.conf ~/.config/compton/c.conf
@@ -28,5 +30,5 @@ cp $SCRIPT_DIR/configs/i3move.sh ~/.config/i3/move.sh
 cp $SCRIPT_DIR/fonts/nerdfont.ttf ~/.local/share/fonts/nerdfont.ttf
 cp $SCRIPT_DIR/rofi/material.rasi ~/.local/share/rofi/themes/material.rasi
 cp -r $SCRIPT_DIR/cursors/. ~/.icons/
-
+cp $SCRIPT_DIR/configs/roficonfig.rasi ~/.config/rofi/config.rasi
 echo "config files installed! (0w0)"
