9cdb9b7547a0985c4feb8f4027bc510e6a591c18
taran <tarandeepsingh399113@gmail.com>
2026-08-26 10:53:39 +0530

    removed redundant icons

--- install.sh
+++ install.sh
@@ -29,7 +29,7 @@ cp -r $SCRIPT_DIR/configs/wallpapers ~/wallpapers
 cp $SCRIPT_DIR/configs/i3move.sh ~/.config/i3/move.sh
 cp $SCRIPT_DIR/fonts/nerdfont.ttf ~/.local/share/fonts/nerdfont.ttf
 cp $SCRIPT_DIR/rofi/material.rasi ~/.local/share/rofi/themes/material.rasi
-cp -r $SCRIPT_DIR/cursors/. ~/.icons/
+cp -r $SCRIPT_DIR/cursors/. ~/.icons/.
 cp $SCRIPT_DIR/configs/roficonfig.rasi ~/.config/rofi/config.rasi
 cp $SCRIPT_DIR/resources/.Xresources ~/.Xrsources
 
